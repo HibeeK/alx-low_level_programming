aef5ec4cae2c9a9dbd7a6be2b66fe0c5d2002597
HibeeK <akintolauser@gmail.com>
2022-07-10 18:32:43 +0000

    c variables

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints whether the last digit of a random number is greater than 5,
+ *	is 0, or it is less than 6 and not 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
+
+	if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	if (m == 0)
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	if (m < 6 && != 0)
+		printf("Last digit of %d is %d annd is less than 6 and not 0\n", n, m);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+0. The pogram will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative
+1. The program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n
