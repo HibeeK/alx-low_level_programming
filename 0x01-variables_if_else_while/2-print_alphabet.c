9eb652654bc40280b75240fe58610942d358f460
HibeeK <akintolauser@gmail.com>
2022-07-10 20:05:51 +0000

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,7 +13,7 @@ int main(void)
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,7 +16,7 @@ int main(void)
 	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
 
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 
