bf633784d6a3fc3a242a0f01ece72bfa04b493ac
HibeeK <akintolauser@gmail.com>
2022-07-10 19:59:55 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar("%c ", c);
+	putchar("\n");
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar("%c ", c);
+
+	for (c = 'A'; c <= 'Z'; ++c)
+		putchar("%c ", c);
+
+	putchar("\n");
+
+	return (0);
+
+}
