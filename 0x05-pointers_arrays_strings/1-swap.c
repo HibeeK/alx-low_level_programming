6d828b85731c0145dc5b0504619f97fb96e38d81
HibeeK <akintolauser@gmail.com>
2022-08-11 17:04:35 +0000

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the value of two integers
+ * @a: pointer to an integer
+ * @b: pointer to another integer
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
+
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,5 @@
 0. 98 Battery st. a function that takes a pointer to an int as parameter and updates the value it points to to 98.
 
+1. Don't swap horses in crossing a stream;  a function that swaps the values of two integers
+
 
