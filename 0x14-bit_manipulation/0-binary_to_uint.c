2e298fd1717ca439c7b9938d7a95bd1de94e4dc5
HibeeK <akintolauser@gmail.com>
2022-08-21 10:04:59 +0000

    fix print_binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,25 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int j;
+	unsigned int ui;
+	int len, base_two;
 
-	j = 0;
 	if (!b)
-		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	return (0);
+	ui = 0;
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] != '0' && b[i] != 1)
+		if (b[len] != '\0' && b[len] != '1')
+		{
 			return (0);
+		}
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		j <<= 1;
-		if (b[i] == '1')
-			j += 1;
-	}
-	return (j);
+	return (ui);
 }
+
