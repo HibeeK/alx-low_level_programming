5e68b7d8606a05f11cd1da3963b3a7952660e915
HibeeK <akintolauser@gmail.com>
2022-08-21 09:27:26 +0000

    all teyasks done

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -5,24 +5,25 @@
  * @b: binary
  * Return: unsigned int
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-    unsigned int j;
+	int i;
+	unsigned int j;
 
-    j = 0;
-    if (!b)
-    return (0);
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != 1)
-        return (0);
-    }
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        j <<= 1;
-        if (b[i] == '1')
-        j += 1;
-    }
-    return (j);
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != 1)
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
