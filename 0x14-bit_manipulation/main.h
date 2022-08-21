a7e587d64a6f02109868f0ad2bf33be32e6349d3
HibeeK <akintolauser@gmail.com>
2022-08-21 09:54:01 +0000

    fix print_binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 unsigned int binary_to_uint(const char *b);
-unsigned long int _power(unsigned int base, unsigned int pow);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
